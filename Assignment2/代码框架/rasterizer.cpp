6bc07b75cad885aaa1925cfedbcc5427ceb753a0
peter <971454032@qq.com>
2023-05-21 20:34:58 +0800

    update hw2

--- Assignment2/代码框架/rasterizer.cpp
+++ Assignment2/代码框架/rasterizer.cpp
@@ -40,10 +40,28 @@ auto to_vec4(const Eigen::Vector3f& v3, float w = 1.0f)
 }
 
 
-static bool insideTriangle(int x, int y, const Vector3f* _v)
-{   
+static bool insideTriangle(float x, float y, const Vector3f* _v)
+{
      // TODO : Implement this function to check if the point (x, y) is inside the triangle represented by _v[0], _v[1], _v[2]
 
+    Eigen::Vector3f p(x, y,_v[0].z());
+    Eigen::Vector3f v0 = _v[0];
+    Eigen::Vector3f v1 = _v[1];
+    Eigen::Vector3f v2 = _v[2];
+
+    Eigen::Vector3f e0 = v1 - v0;
+    Eigen::Vector3f e1 = v2 - v1;
+    Eigen::Vector3f e2 = v0 - v2;
+
+    Eigen::Vector3f vp0 = p - v0;
+    Eigen::Vector3f vp1 = p - v1;
+    Eigen::Vector3f vp2 = p - v2;
+
+    float z0 = e0.cross(vp0).z();
+    float z1 = e1.cross(vp1).z();
+    float z2 = e2.cross(vp2).z();
+
+    return (z0 > 0 && z1 > 0 && z2 > 0) || (z0 < 0 && z1 < 0 && z2 < 0);
 }
 
 static std::tuple<float, float, float> computeBarycentric2D(float x, float y, const Vector3f* v)
@@ -106,7 +124,7 @@ void rst::rasterizer::draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf
 //Screen space rasterization
 void rst::rasterizer::rasterize_triangle(const Triangle& t) {
     auto v = t.toVector4();
-    
+
     // TODO : Find out the bounding box of current triangle.
     // iterate through the pixel and find if the current pixel is inside the triangle
 
@@ -118,6 +136,68 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t) {
     //z_interpolated *= w_reciprocal;
 
     // TODO : set the current pixel (use the set_pixel function) to the color of the triangle (use getColor function) if it should be painted.
+    bool UseSuperSampling = true;
+
+    std::vector<Eigen::Vector3f> SampleOffsets={
+        {0.25,0.25,0},
+        {0.25,0.75,0},
+        {0.75,0.25,0},
+        {0.75,0.75,0}
+    };
+
+    // Create bbox
+    int x_min = std::floor(std::min(v[0].x(), std::min(v[1].x(), v[2].x())));
+    int x_max = std::ceil(std::max(v[0].x(), std::max(v[1].x(), v[2].x())));
+    int y_min = std::floor(std::min(v[0].y(), std::min(v[1].y(), v[2].y())));
+    int y_max = std::ceil(std::max(v[0].y(), std::max(v[1].y(), v[2].y())));
+
+    // Iterate through the pixel and find if the current pixel is inside the triangle
+    for(int i = x_min; i < x_max; i++){
+        for(int j = y_min; j < y_max; j++){
+            int index = get_index(i, j);
+            int IsInTriangleCount = 0;
+            int IsDontBeCover = 0;
+            int l = 0;
+
+            if(!UseSuperSampling){
+                if(insideTriangle(i + 0.5f, j + 0.5f, t.v)){
+                    auto[alpha, beta, gamma] = computeBarycentric2D(i + 0.5f, j + 0.5f, t.v);
+                    float w_reciprocal = 1.0/(alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
+                    float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
+                    z_interpolated *= w_reciprocal;
+
+                    if(z_interpolated < depth_buf[index]){
+                        depth_buf[index] = z_interpolated;
+                        set_pixel(Eigen::Vector3f(i, j, 1.0f), t.getColor());
+                    }
+                }
+            }else{
+                //supersampling
+                for(auto & Offset : SampleOffsets){
+                    float x = i + Offset.x();
+                    float y = j + Offset.y();
+                    if(insideTriangle(x, y, t.v)){
+                        auto[alpha, beta, gamma] = computeBarycentric2D(x, y, t.v);
+                        float w_reciprocal = 1.0f / (alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
+                        float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
+                        z_interpolated *= w_reciprocal;
+
+                        if(z_interpolated < supersample_depth_buf[index][l]){
+                            supersample_depth_buf[index][l] = z_interpolated;
+                            IsDontBeCover++;
+                        }
+                        IsInTriangleCount++;
+                    }
+                    l++;
+                }
+                Vector3f color = t.getColor() * IsInTriangleCount / 4.0f;
+                if(IsDontBeCover > 0)
+                {
+                    set_pixel(Vector3f(i,j,0),color);
+                }
+            }
+        }
+    }
 }
 
 void rst::rasterizer::set_model(const Eigen::Matrix4f& m)
@@ -143,7 +223,10 @@ void rst::rasterizer::clear(rst::Buffers buff)
     }
     if ((buff & rst::Buffers::Depth) == rst::Buffers::Depth)
     {
+        std::array<float, 4> inf;
+        inf.fill(std::numeric_limits<float>::infinity());
         std::fill(depth_buf.begin(), depth_buf.end(), std::numeric_limits<float>::infinity());
+        std::fill(supersample_depth_buf.begin(), supersample_depth_buf.end(), inf);
     }
 }
 
@@ -151,6 +234,8 @@ rst::rasterizer::rasterizer(int w, int h) : width(w), height(h)
 {
     frame_buf.resize(w * h);
     depth_buf.resize(w * h);
+    supersample_color_buf.resize(w * h);
+    supersample_depth_buf.resize(w * h);
 }
 
 int rst::rasterizer::get_index(int x, int y)
